4bafa4f4309891e77b01d28cdf88737c8a9306e3
sehundpark <sehunpark123@gmail.com>
2024-10-10 01:49:44 -0700

    Added 6 projects to projects array.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,31 +1,66 @@
 import styled from "styled-components";
 
-export const ProjectCard = ({ image, title, description, link }) => {
+export const ProjectCard = ({ image, title, description, link, demo }) => {
   return (
     <StyledCard>
       <StyledCardTitle>{title}</StyledCardTitle>
-      <img src={image} alt={title} />
+      <StyledImage src={image} alt={title} />
       <StyledCardDescription>{description}</StyledCardDescription>
-      <button>
-        <a href={link}>View Project</a>
-      </button>
+      <StyledButtonContainer>
+        <StyledButton>
+          <StyledLink href={link} target="_blank" rel="noopener noreferrer">
+            GitHub
+          </StyledLink>
+        </StyledButton>
+        <StyledButton>
+          <StyledLink href={demo} target="_blank" rel="noopener noreferrer">
+            Demo
+          </StyledLink>
+        </StyledButton>
+      </StyledButtonContainer>
     </StyledCard>
   );
 };
 
 const StyledCard = styled.div`
   display: flex;
-  width: 42%;
-  align-items: center;
   flex-direction: column;
   padding: 12px;
   margin: 12px;
+  width: 45%;
+  border: 2px solid black;
+  border-radius: 10px;
 `;
 
-const StyledCardTitle = styled.div`
-  //
+const StyledCardTitle = styled.h3`
+  // Add your styles here
 `;
 
-const StyledCardDescription = styled.div`
-  //
+const StyledImage = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
 `;
+
+const StyledCardDescription = styled.p`
+  // Add your styles here
+`;
+
+const StyledButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+const StyledButton = styled.button`
+  background-color: white;
+  border: 2px solid black;
+  width: fit-content;
+`;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  color: inherit;
+`;
+
+export default ProjectCard;
