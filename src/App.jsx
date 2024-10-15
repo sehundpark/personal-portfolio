858a14b3e9c2520b35d9fb9712e8262e95839967
sehundpark <sehunpark123@gmail.com>
2024-10-14 20:35:54 -0700

    Fixed typo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./pages/Navbar.jsx";
+import { Navbar } from "./pages/Navbar";
 import { Footer } from "./pages/Footer";
 import { Home } from "./pages/Home";
 import { Works } from "./pages/Works";
